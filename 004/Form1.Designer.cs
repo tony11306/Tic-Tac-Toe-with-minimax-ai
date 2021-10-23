6935f9ba303245dbb856175ac0fc7706cde28ba9
Tony11306 <tony20020507@gmail.com>
2021-10-23 17:09:57 +0800

    first commit

---
+++ 004/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace _004 {
+    partial class Form1 {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.currentPlayerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(352, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 90);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(467, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 90);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(234, 177);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 90);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(352, 177);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 90);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(467, 177);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(97, 90);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(234, 283);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(97, 90);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(352, 283);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(97, 90);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(467, 283);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(97, 90);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // currentPlayerLabel
+            // 
+            this.currentPlayerLabel.AutoSize = true;
+            this.currentPlayerLabel.Location = new System.Drawing.Point(355, 42);
+            this.currentPlayerLabel.Name = "currentPlayerLabel";
+            this.currentPlayerLabel.Size = new System.Drawing.Size(94, 15);
+            this.currentPlayerLabel.TabIndex = 9;
+            this.currentPlayerLabel.Text = "current player: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.currentPlayerLabel);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label currentPlayerLabel;
+    }
+}
+
---
+++ 004/Form1.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _004 {
+    // 寫一寫發現耦合度超高...
+    public partial class Form1 : Form {
+
+        private Game game = null;
+        private HumanPlayer humanPlayer = null;
+        private AI ai = null;
+        private List<List<Button>> buttons;
+        private Timer timer = new Timer();
+        public Form1() {
+            InitializeComponent();
+            buttons = new List<List<Button>> { 
+                new List<Button> { button1, button2, button3 },
+                new List<Button> { button4, button5, button6 },
+                new List<Button> { button7, button8, button9 },
+            };
+            humanPlayer = new HumanPlayer(Symbol.CIRCLE);
+            ai = new AI(Symbol.CROSS, buttons);
+            game = new Game(ai, humanPlayer); // 前 O player, 後 X player, 若要對調記得修改
+            for (int i = 0; i < Game.BOARD_SIZE; ++i) {
+                for (int j = 0; j < Game.BOARD_SIZE; ++j) {
+                    buttons[i][j].Text = "";
+                    // can't directly use i j for some reasons, I think it's probably related to reference stuff >:(
+                    int row = i; 
+                    int col = j;
+                    buttons[i][j].Click += (sender, e) => { 
+                        game.buttonEvent(sender, e, row, col);
+                        if (game.checkIsGameOver()) {
+                            return;
+                        }
+                        currentPlayerLabel.Text = "Current Player: " + (game.getCurrentPlayer() == humanPlayer ? "You" : "AI");
+                    };
+                }
+            }
+
+            timer.Interval = 100;
+            timer.Tick += timerTick;
+            currentPlayerLabel.Text = "Current Player: " + (game.getCurrentPlayer() == humanPlayer ? "You" : "AI");
+            timer.Start();
+
+        }
+
+
+
+        private void timerTick(object sender, EventArgs e) {
+
+            if (game.checkIsGameOver()) {
+                timer.Stop();
+                if (game.getWinner() == null) {
+                    currentPlayerLabel.Text = "Tie game";
+                } else {
+                    currentPlayerLabel.Text = "Winner: " + (game.getWinner() == humanPlayer ? "You" : "AI");
+                }
+                return;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            if (game.getCurrentPlayer().isAI()) {
+                ai.play(game);
+            }
+        }
+    }
+
+    public enum Symbol {
+        EMPTY = 0,
+        CROSS = 'O',
+        CIRCLE = 'X'
+    }
+
+    public interface IPlayer {
+        void play(Game game);
+        bool isAI();
+
+        Symbol getSymbol();
+
+    }
+
+    public partial class HumanPlayer : IPlayer {
+
+        private Symbol symbolSide;
+        public HumanPlayer(Symbol symbolSide) {
+            this.symbolSide = symbolSide;
+        }
+        public void play(Game game) {
+
+        }
+
+        public bool isAI() {
+            return false;
+        }
+
+        public Symbol getSymbol() {
+            return symbolSide;
+        }
+
+
+    }
+
+    public partial class AI : IPlayer {
+        private Symbol symbolSide;
+        private int DEPTH = 9;
+        private List<List<Button>> buttons;
+        public AI(Symbol symbolSide, List<List<Button>> buttons) {
+            this.symbolSide = symbolSide;
+            this.buttons = buttons;
+        }
+        public int evaluate(Game game, bool isMaxing=false, int currentDepth = 0) { // minimax algorithm
+
+            Symbol[][] board = game.getBoard();
+            if (currentDepth >= DEPTH) {
+                return 0;
+            }
+
+            if (isLose(game)) {
+                return -100;
+            } else if (isWin(game)) {
+                return 50;
+            } else if (isTie(board)) {
+                return 1;
+            }
+
+            List<int> scores = new List<int>();
+            for (int i = 0; i < Game.BOARD_SIZE; ++i) {
+                for (int j = 0; j < Game.BOARD_SIZE; ++j) {
+                    if (board[i][j] == Symbol.EMPTY) {
+                        if (getSymbol() == Symbol.CROSS) {
+                            board[i][j] = isMaxing ? Symbol.CROSS : Symbol.CIRCLE;
+                        } else {
+                            board[i][j] = isMaxing ? Symbol.CIRCLE : Symbol.CROSS;
+                        }
+                        scores.Add(evaluate(game, !isMaxing, currentDepth+1));
+                        board[i][j] = Symbol.EMPTY;
+                    }
+                }
+            }
+            if (isMaxing) {
+                return scores.Max();
+            }
+            return scores.Min();
+        }
+
+        public bool isWin(Game game) {
+            return game.getWinner() == this;
+        }
+
+        public bool isLose(Game game) {
+            IPlayer winner = game.getWinner();
+            return winner != null && winner != this;
+        }
+
+        public bool isTie(Symbol[][] board) {
+            for (int i = 0; i < Game.BOARD_SIZE; ++i) {
+                for (int j = 0; j < Game.BOARD_SIZE; ++j) {
+                    if (board[i][j] == Symbol.EMPTY) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public void play(Game game) {
+            // score row col
+            List<Tuple<int, int, int>> possibleMoves = new List<Tuple<int, int, int>>();
+            Symbol[][] board = game.getBoard();
+            for (int i = 0; i < Game.BOARD_SIZE; ++i) {
+                for (int j = 0; j < Game.BOARD_SIZE; ++j) {
+                    if (board[i][j] == Symbol.EMPTY) {
+                        board[i][j] = getSymbol();
+                        Tuple<int, int, int> possibleMove = new Tuple<int, int, int>(evaluate(game), i, j);
+                        board[i][j] = Symbol.EMPTY;
+                        possibleMoves.Add(possibleMove);
+                    }
+                }
+            }
+            Console.WriteLine("----- AI Decision -----");
+            foreach (Tuple<int, int, int> m in possibleMoves) {
+                Console.WriteLine("move " + m.Item2 + "/" + m.Item3 + ", score: " + m.Item1);
+            }
+
+            Tuple<int, int, int> bestMove = possibleMoves.Max();
+            buttons[bestMove.Item2][bestMove.Item3].PerformClick();
+            // Console.WriteLine("AI Clicked" + bestMove.Item2 + " " + bestMove.Item3);
+
+        }
+        public bool isAI() {
+            return true;
+        }
+        public Symbol getSymbol() {
+            return symbolSide;
+        }
+    }
+
+    public partial class Game {
+        public const int BOARD_SIZE = 3;
+        private Symbol[][] board = new Symbol[BOARD_SIZE][];
+        private IPlayer crossPlayer = null; // cross side
+        private IPlayer circlePlayer = null; // circle side
+        private IPlayer currentPlayer = null;
+        private bool isGameOver = false;
+
+        /*
+         (0,0), (0,1), (0,2)
+         (1,0), (1,1), (1,2)
+         (2,0), (2,1), (2,2)
+         */
+
+        public Game(IPlayer crossPlayer, IPlayer circlePlayer) { // 前 O 後 X 重要!!
+            this.crossPlayer = crossPlayer;
+            this.circlePlayer = circlePlayer;
+            this.currentPlayer = this.circlePlayer;
+            for (int i = 0; i < BOARD_SIZE; ++i) {
+                board[i] = new Symbol[BOARD_SIZE];
+            }
+        }
+
+        public void buttonEvent(object sender, EventArgs e, int row, int col) {
+
+            if (isGameOver) {
+                return;
+            }
+
+            if (board[row][col] != Symbol.EMPTY) {
+                return;
+            }
+            board[row][col] = currentPlayer.getSymbol();
+            ((Button)sender).Text = board[row][col] == Symbol.CIRCLE ? "O" : "X";
+
+            if (getWinner() != null || isTie()) {
+                isGameOver = true;
+                return;
+            }
+
+            switchCurrentPlayer();
+
+            if (currentPlayer.isAI()) {
+                ((AI)currentPlayer).play(this);
+            }
+
+            
+            
+            
+
+        }
+
+        private void switchCurrentPlayer() {
+            currentPlayer = currentPlayer == crossPlayer ? circlePlayer : crossPlayer;
+        }
+        private bool isCrossWin() { // O(n^2)
+            // check all rows
+            for (int row = 0; row < BOARD_SIZE; ++row) {
+                bool isWin = true;
+                for (int col = 0; col < BOARD_SIZE; ++col) {
+                    if (board[row][col] != Symbol.CROSS) {
+                        isWin = false;
+                        break;
+                    }
+                }
+                if (isWin) {
+                    return true;
+                }
+            }
+
+            // check all cols
+            for (int col = 0; col < BOARD_SIZE; ++col) {
+                bool isWin = true;
+                for (int row = 0; row < BOARD_SIZE; ++row) {
+                    if (board[row][col] != Symbol.CROSS) {
+                        isWin = false;
+                        break;
+                    }
+                }
+                if (isWin) {
+                    return true;
+                }
+            }
+
+            // check diagonals
+            bool isDiagonal = true;
+            for (int i = 0; i < BOARD_SIZE; ++i) {
+                if (board[i][i] != Symbol.CROSS) {
+                    isDiagonal = false;
+                    break;
+                }
+            }
+
+            if (isDiagonal) {
+                return true;
+            }
+            isDiagonal = true;
+            for (int i = 0; i < BOARD_SIZE; ++i) {
+                if (board[BOARD_SIZE - i - 1][i] != Symbol.CROSS) {
+                    isDiagonal = false;
+                    break;
+                }
+            }
+
+            if (isDiagonal) {
+                return true;
+            }
+
+            return false;
+        }
+        private bool isCircleWin() { // O(n^2)
+            // check all rows
+            for (int row = 0; row < BOARD_SIZE; ++row) {
+                bool isWin = true;
+                for (int col = 0; col < BOARD_SIZE; ++col) {
+                    if (board[row][col] != Symbol.CIRCLE) {
+                        isWin = false;
+                        break;
+                    }
+                }
+                if (isWin) {
+                    return true;
+                }
+            }
+
+            // check all cols
+            for (int col = 0; col < BOARD_SIZE; ++col) {
+                bool isWin = true;
+                for (int row = 0; row < BOARD_SIZE; ++row) {
+                    if (board[row][col] != Symbol.CIRCLE) {
+                        isWin = false;
+                        break;
+                    }
+                }
+                if (isWin) {
+                    return true;
+                }
+            }
+
+            // check diagonals
+            bool isDiagonal = true;
+            for (int i = 0; i < BOARD_SIZE; ++i) {
+                if (board[i][i] != Symbol.CIRCLE) {
+                    isDiagonal = false;
+                    break;
+                }
+            }
+
+            if (isDiagonal) {
+                return true;
+            }
+
+            isDiagonal = true;
+
+            for (int i = 0; i < BOARD_SIZE; ++i) {
+                if (board[BOARD_SIZE - i - 1][i] != Symbol.CIRCLE) {
+                    isDiagonal = false;
+                    break;
+                }
+            }
+
+            if (isDiagonal) {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool isTie() {
+            for (int i = 0; i < Game.BOARD_SIZE; ++i) {
+                for (int j = 0; j < Game.BOARD_SIZE; ++j) {
+                    if (board[i][j] == Symbol.EMPTY) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public IPlayer getWinner() {
+            if (isCrossWin()) {
+                return crossPlayer;
+            } else if (isCircleWin()) {
+                return circlePlayer;
+            }
+
+            return null;
+        }
+
+        public Symbol[][] getBoard() {
+            return board;
+        }
+        public IPlayer getCurrentPlayer() {
+            return currentPlayer;
+        }
+
+        public bool checkIsGameOver() {
+            return isGameOver;
+        }
+
+
+
+    }
+    
+}
